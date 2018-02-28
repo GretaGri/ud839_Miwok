f8ba271bdebea49a5ddb744f2a3e57ad8a233b7d
Greta Gri <greta.klimaite@gmail.com>
2018-02-28 14:15:28 +0200

    Added an array of words (numbers from 1 to 10) in NumbersActivity.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -9,5 +9,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Creates a string array with ten words numbers from 1 to 10.
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
     }
 }
